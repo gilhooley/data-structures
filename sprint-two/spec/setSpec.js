31c5358720d97b5f1d6f4651f4fbb043b0e6b402
KAG <k.gilhooley@gmail.com>
2014-09-13 14:50:43 -0700

    set can handle any type of input

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -24,4 +24,29 @@ describe('set', function() {
     expect(set.contains("Mel Gibson")).to.equal(false);
   });
 
+  it('should be able to handle integers', function() {
+    set.add(4);
+    expect(set.contains(4)).to.equal(true);
+    set.add(5);
+    expect(set.contains(5)).to.equal(true);
+    set.remove(4);
+    expect(set.contains(4)).to.equal(false);
+  });
+
+    it('should be able to handle inputs of any type', function() {
+    set.add(true);
+    expect(set.contains(true)).to.equal(true);
+     var obj = {a:4};
+    set.add(obj);
+    expect(set.contains(obj)).to.equal(true);
+     var fnc  = function (val) {return val;} 
+    set.add(fnc);
+    expect(set.contains(fnc)).to.equal(true);
+    set.remove({a:4});
+    expect(set.contains({a:4})).to.equal(false);
+    set.remove(fnc);
+    expect(set.contains(fnc)).to.equal(false);
+
+  });
+
 });
